4477aa63bccf9df01c30d003884aebe3ca3f2b65
John Bryan Valle <jbvalle.jv@gmail.com>
2022-10-17 03:32:07 +0200

    finished gpio initialization

--- src/main.c
+++ src/main.c
@@ -7,8 +7,18 @@ GPIOx_t     * const GPIOC   = (GPIOx_t  *)  0x40020800;
 SYSCFG_t    * const SYSCFG  = (SYSCFG_t *)  0x40013800;
 EXTI_t      * const EXTI    = (EXTI_t   *) 0x40013C00;
 
+void GPIOx_INIT(void){
+    /** Enable GPIOA and C **/
+    RCC->RCC_AHB1ENR |= ((1 << 2) | (1 << 0));
+    /** Set GPIOA as OUTPUT **/
+    GPIOA->GPIOx_MODER &= ~(3 << (5 * 2));
+    GPIOA->GPIOx_MODER |=  (1 << (5 * 2));
+    /** Set GPIOC as INPUT for IRQ **/
+    GPIOC->GPIOx_MODER &= ~(3 << (13 * 2));
+}
 
 int main(void){
 
+    GPIOx_INIT();
     for(;;);
 }
